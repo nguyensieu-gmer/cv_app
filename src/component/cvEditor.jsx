aa6eec117ed2adb39a33bf72c2c077465847df7d
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-07-04 21:08:24 +0700

    Add props for input

--- src/component/cvEditor.jsx
+++ src/component/cvEditor.jsx
@@ -1,23 +1,47 @@
-export function Form({ personalInfor, education, experience }) {
+export function Form({ personalInfor, education, experience, actions }) {
   const { name, email, address, phone } = personalInfor;
   return (
     <div>
       <form action="">
         <div>
           <label htmlFor="userName">Name:</label>
-          <input name="name" type="text" value={name} id="userName" />
+          <input
+            onChange={actions.handleChangePersonalInfor}
+            name="name"
+            type="text"
+            value={name}
+            id="userName"
+          />
         </div>
         <div>
           <label htmlFor="userEmail">Email:</label>
-          <input type="email" name="email" id="userEmail" value={email} />
+          <input
+            onChange={actions.handleChangePersonalInfor}
+            type="email"
+            name="email"
+            id="userEmail"
+            value={email}
+          />
         </div>
         <div>
           <label htmlFor="userTel">Phone:</label>
-          <input type="tel" name="phone" id="userTel" value={phone} />
+          <input
+            onChange={actions.handleChangePersonalInfor}
+            type="tel"
+            name="phone"
+            id="userTel"
+            value={phone}
+          />
         </div>
         <div>
           <label htmlFor="userLocal">Address</label>
-          <input name="address" type="text" value={address} id="userLocal" />
+          <input
+            onChange={actions.handleChangePersonalInfor}
+            name="address"
+            type="text"
+            value={address}
+            id="userLocal"
+          />
         </div>
       </form>
       <div>Education</div>
@@ -26,14 +50,14 @@ export function Form({ personalInfor, education, experience }) {
           return <EduItem key={item.id} {...item} />;
         })}
       </ul>
-      <div>add education</div>
+      <button>add education</button>
       <div>Experience</div>
       <ul>
         {experience.map((item) => {
           return <ExpItem key={item.id} {...item} />;
         })}
       </ul>
-      <div>add experience</div>
+      <button>add experience</button>
     </div>
   );
 }
