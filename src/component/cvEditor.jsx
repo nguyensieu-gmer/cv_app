2f7a84648de6d3dfa22f165d8935d0e5d3cc0c47
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-07-05 12:05:42 +0700

    Add form form Addtion education infor

--- src/component/cvEditor.jsx
+++ src/component/cvEditor.jsx
@@ -1,5 +1,13 @@
+import { useState } from "react";
+
 export function Form({ personalInfor, education, experience, actions }) {
+  const [eduIsOpen, setEduForm] = useState(false);
   const { name, email, address, phone } = personalInfor;
+
+  function handleOpenEduForm(value) {
+    setEduForm(value);
+  }
+
   return (
     <div>
       <form action="">
@@ -46,11 +54,15 @@ export function Form({ personalInfor, education, experience, actions }) {
       </form>
       <div>Education</div>
       <ul>
-        {education.map((item) => {
-          return <EduItem key={item.id} {...item} />;
-        })}
+        {eduIsOpen ? (
+          <EduForm handleChange={handleOpenEduForm} />
+        ) : (
+          education.map((item) => {
+            return <EduItem key={item.id} {...item} />;
+          })
+        )}
       </ul>
-      <button>add education</button>
+      <button onClick={() => handleOpenEduForm(true)}>add education</button>
       <div>Experience</div>
       <ul>
         {experience.map((item) => {
@@ -68,4 +80,46 @@ function EduItem({ name }) {
 
 function ExpItem({ name }) {
   return <li>{name}</li>;
+}
+
+// {
+//       name: "Hanoi University",
+//       from: "2024",
+//       to: "2025",
+//       major: "Software Engineer",
+//       address: "Linh Dam, Ha Noi",
+//       visible: true,
+//       id: crypto.randomUUID(),
+//     },
+
+function EduForm({ handleChange }) {
+  return (
+    <form action="">
+      <div>
+        <label htmlFor="eduFrom">From: </label>
+        <input type="text" name="from" id="eduFrom" />
+      </div>
+      <div>
+        <label htmlFor="eduTo">To: </label>
+        <input type="text" name="to" id="eduTo" />
+      </div>
+      <div>
+        <label htmlFor="eduName">Name: </label>
+        <input type="text" name="name" id="eduName" />
+      </div>
+      <div>
+        <label htmlFor="eduAddress">Location: </label>
+        <input type="text" name="address" id="eduAddress" />
+      </div>
+      <div>
+        <label htmlFor="eduMajor">Major: </label>
+        <input type="text" name="major" id="eduMajor" />
+      </div>
+      <button type="reset">Reset</button>
+      <button onClick={() => handleChange(false)} type="button">
+        Cancel
+      </button>
+      <button type="submit">Add</button>
+    </form>
+  );
 }
