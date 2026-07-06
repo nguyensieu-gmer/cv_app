8f211700e3da1de5aef9d0a8958d73bb2f495c59
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-07-06 21:34:30 +0700

    Add remixicon

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./style/index.css";
 import "./style/App.css";
+import "remixicon/fonts/remixicon.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
