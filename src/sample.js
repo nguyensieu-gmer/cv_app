af7f3ce2bb77264a593ecad5f1f5b4ef5c37e846
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-07-06 21:57:15 +0700

    Add description infor for sample

--- src/sample.js
+++ src/sample.js
@@ -33,6 +33,8 @@ export const data = {
       positionTitle: "UI design",
       location: "Thai Nguyen VN",
       visible: true,
+      desciption:
+        "Assisted in conducting user research to understand user needs, behaviors, and pain points. Supported data collection through surveys, interviews, and usability testing, and contributed to analyzing insights to improve user experience design. Collaborated with the design team to translate research findings into actionable recommendations.",
       id: crypto.randomUUID(),
     },
     {
@@ -42,6 +44,8 @@ export const data = {
       positionTitle: "Biotech Engineer",
       location: "Raccoon City",
       visible: true,
+      desciption:
+        "Designed and prototyped user interface patterns for various clients in various industries, ranging from self-service apps within the telecommunications-sector to mobile games for IOS and Android",
       id: crypto.randomUUID(),
     },
   ],
